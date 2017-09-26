d9cd41d408731e0aed714003a39a83b2d30185a2
Denys Tymoshkevych <dtimoshkevich@egp.com.ua>
2017-09-26 10:41:38 +0300

    finished

--- src/main/java/ua/com/fdi/demo/config/ClientOAuth2Application.java
+++ src/main/java/ua/com/fdi/demo/config/ClientOAuth2Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan(value = {"ua.com.fdi.demo.controller"})
 public class ClientOAuth2Application {
     public static void main(String[] args) {
         SpringApplication.run(ClientOAuth2Application.class, args);
